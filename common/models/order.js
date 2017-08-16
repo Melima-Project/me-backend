3e76ec550049cbeec4e74b8dd4a3285fa4aad343
Anonymous <admin@hda.me>
2017-08-16 11:42:04 +0000

    Upload Melima Core Base

---
+++ common/models/order.js
@@ -0,0 +1,73 @@
+'use strict';
+
+module.exports = function(Order) {
+    Order.validatesPresenceOf('Table', 'Count', 'SpecialWishes');
+    Order.validatesLengthOf('Name', {min: 3, max: 64, message: {min: 'Name is too short', max: 'Name is too long'}});
+    Order.validateAsync('Price', validatePrice, {message: 'Price should be > 1'});
+    Order.validateAsync('CountAvailable', validateCountAvailable, {message: 'Count should be > 1'});
+    Order.validateAsync('DiscountPercent', validateDiscountPercent, {message: 'Minimum 10% from the price'});
+    Order.validatesLengthOf('Description', {min: 3, max: 600, message: {min: 'Text too short', max: 'Use shorter text'}});
+    Order.validatesLengthOf('SerialNumber', {min: 0, max: 20, message: {min: 'SerialNumber is too short', max: 'SerialNumber is too long'}});
+    Order.validateAsync('Table', validateTable, {message: 'Table should be >= 1'});
+    Order.validateAsync('Count', validateCount, {message: 'Count should be >= 1'});
+    Order.validatesLengthOf('ClientName', {min: 3, max: 64, message: {min: 'Name is too short', max: 'Name is too long'}});
+    Order.validatesLengthOf('ClientLastName', {min: 3, max: 64, message: {min: 'Name is too short', max: 'Name is too long'}});
+    Order.validatesLengthOf('PhoneNumber', {min: 6, max: 15, message: {min: 'Too short phone number', max: 'Too long for phone number'}});
+    Order.validatesLengthOf('SpecialWishes', {min: 3, max: 600, message: {min: 'Text too short', max: 'Use shorter text'}});
+
+function validatePrice(err, done) {
+    var Price = this.Price;
+    process.nextTick(function () {
+        if (Price >= 1 && Price <= 100000) {
+        done();
+        } else {
+          console.log(err, "Incorrect Price: " +Price+ ", request rejected");
+        }
+    });
+}
+
+function validateCountAvailable(err, done) {
+    var CountAvailable = this.CountAvailable;
+    process.nextTick(function () {
+        if (CountAvailable >= 1 && CountAvailable <= 10000) {
+        done();
+        } else {
+          console.log(err, "Incorrect CountAvailable: " +CountAvailable+ ", request rejected");
+        }
+    });
+}
+
+function validateDiscountPercent(err, done) {
+    var DiscountPercent = this.DiscountPercent;
+    process.nextTick(function () {
+        if (DiscountPercent >= 0.1 && DiscountPercent <= 1) {
+        done();
+        } else {
+          console.log(err, "Incorrect DiscountPercent: " +DiscountPercent+ ", request rejected");
+        }
+    });
+}
+
+function validateTable(err, done) {
+    var Table = this.Table;
+    process.nextTick(function () {
+        if (Table >= 1 && Table <= 100) {
+        done();
+        } else {
+          console.log(err, "Incorrect Table: " +Table+ ", request rejected");
+        }
+    });
+}
+
+function validateCount(err, done) {
+    var Count = this.Count;
+    process.nextTick(function () {
+        if (Count >= 1 && Count <= 100) {
+        done();
+        } else {
+          console.log(err, "Incorrect Count: " +Count+ ", request rejected");
+        }
+    });
+}
+
+};
---
+++ common/models/product.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = function(Product) {
+    
+    Product.validatesPresenceOf('Name', 'Price', 'Description');
+    Product.validatesLengthOf('Name', {min: 3, max: 64, message: {min: 'Name is too short', max: 'Name is too long'}});
+    Product.validateAsync('Price', validatePrice, {message: 'Price should be > 1'});
+    Product.validateAsync('CountAvailable', validateCountAvailable, {message: 'Count should be > 1'});
+    Product.validateAsync('DiscountPercent', validateDiscountPercent, {message: 'Minimum 10% from the price'});
+    Product.validatesLengthOf('Description', {min: 3, max: 600, message: {min: 'Text too short', max: 'Use shorter text'}});
+    Product.validatesLengthOf('SerialNumber', {min: 0, max: 20, message: {min: 'SerialNumber is too short', max: 'SerialNumber is too long'}});
+    
+function validatePrice(err, done) {
+    var Price = this.Price;
+    process.nextTick(function () {
+        if (Price >= 1 && Price <= 100000) {
+        done();
+        } else {
+          console.log(err, "Incorrect Price: " +Price+ ", request rejected");
+        }
+    });
+}
+
+function validateCountAvailable(err, done) {
+    var CountAvailable = this.CountAvailable;
+    process.nextTick(function () {
+        if (CountAvailable >= 1 && CountAvailable <= 10000) {
+        done();
+        } else {
+          console.log(err, "Incorrect CountAvailable: " +CountAvailable+ ", request rejected");
+        }
+    });
+}
+
+function validateDiscountPercent(err, done) {
+    var DiscountPercent = this.DiscountPercent;
+    process.nextTick(function () {
+        if (DiscountPercent >= 0.1 && DiscountPercent <= 1) {
+        done();
+        } else {
+          console.log(err, "Incorrect DiscountPercent: " +DiscountPercent+ ", request rejected");
+        }
+    });
+}
+
+};
